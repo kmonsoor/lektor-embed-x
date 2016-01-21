22ac510e0cfc9f79a65092c8f27c2628004968b8
Khaled Monsoor <kmonsoor@users.noreply.github.com>
2016-01-21 22:25:31 +0600

    initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='lektor-embed-x',
+    version='0.1',
+    author=u'Khaled Monsoor',
+    author_email='k@kmonsoor.com',
+    license='MIT',
+    py_modules=['lektor_embed_x'],
+    requires=['lektor', 'embedx']
+    entry_points={
+        'lektor.plugins': [
+            'embed-x = lektor_embed_x:EmbedXPlugin',
+        ]
+    }
+)
