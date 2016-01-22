7eede3f52b80eacc9ef1f78aad262fc3b0104067
Low Kian Seong <lowks@users.noreply.github.com>
2016-01-22 12:37:01 +0800

    Adding description to setup.py
    Adding `description` to `setup.py`

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='0.1',
     author=u'Khaled Monsoor',
     author_email='k@kmonsoor.com',
+    description='Enables embedding of web-contents from popular sites in MarkDown(*.md) on Lektor CMS',
     license='MIT',
     url='https://github.com/kmonsoor/lektor-embed-x',
     py_modules=['lektor_embed_x'],
