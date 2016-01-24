e0233b0871cd123789e2abecaff229313bacd096
Khaled Monsoor <k@kmonsoor.com>
2016-01-24 23:23:17 +0600

    first working version

--- lektor_embed_x.py
+++ lektor_embed_x.py
@@ -1,41 +1,37 @@
 # -*- coding: utf-8 -*-
-from string import strip
+"""
+    lektor-embed-x
+    ~~~~~~~~~~~~~~
+    Simply embed rich contents from popular sites in Lektor-generated pages
+    :copyright: (c) 2016 by Khaled Monsoor
+    :license: The MIT License
+"""
 
-from markupsafe import Markup
-from lektor.pluginsystem import Plugin
 from embedx import OnlineContent
+from lektor.pluginsystem import Plugin
+from markupsafe import Markup
 
-embedx_marker = '#!#'
+__version__ = '0.1.2'
+__author__ = 'Khaled Monsoor <k@kmonsoor.com>'
 
-EMBED_FAILED_SCRIPT = \
-'''<div class="embedx-notsupported"><p><a href="%(URL)s">%(URL)s</a></p></div>'''
 
 class EmbedXMixin(object):
-
-    def paragraph(self, text):
-        if not text.strip().startswith(embedx_marker):
-            return super(EmbedXMixin, self).paragraph(text)
-        content_url = (strip(text)[3:]).strip()
-
-        try:
-            content = OnlineContent(content_url)
-            return Markup(content.get_embed_code())
-        except NotImplementedError:
-            # should emit this msg when generating
-            print 'The hosting site or the specific content is not supported yet'
-            # failed to generate embeddable code, so returning plain URL
-            return Markup(EMBED_FAILED_SCRIPT % {'URL': content_url})
+    def autolink(self, link, is_email):
+        if is_email:
+            return super(EmbedXMixin, self).autolink(link, True)
+        else:
+            try:
+                content = OnlineContent(link)
+                # print content.get_embed_code()
+                return Markup(content.get_embed_code())
+            except (NotImplementedError, ValueError):
+                print 'This host or this specific content is not supported yet. link: {0}'.format(link)
+                return super(EmbedXMixin, self).autolink(link, False)
 
 
 class EmbedXPlugin(Plugin):
     name = u'lektor-embed-x'
-    description = u'Embed rich content from popular sites in Lektor-generated pages'
-
-#   def on_process_template_context(self, context, **extra):
-#       def test_function():
-#           return 'Value from plugin %s' % self.name
-#       context['test_function'] = test_function
-
+    description = u'Simply embed rich content from popular sites in Lektor-generated pages'
 
     def on_markdown_config(self, config, **extra):
         config.renderer_mixins.append(EmbedXMixin)
--- setup.py
+++ setup.py
@@ -1,8 +1,21 @@
+from codecs import open
+from os import path
+
 from setuptools import setup
+
+# Load the README.md to `long_description`
+here = path.abspath(path.dirname(__file__))
+try:
+    from pypandoc import convert
+
+    long_description = convert('README.md', 'rst')
+except(OSError, IOError, ImportError):
+    with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = f.read()
 
 setup(
     name='lektor-embed-x',
-    version='0.1',
+    version='0.1.2',
     author=u'Khaled Monsoor',
     author_email='k@kmonsoor.com',
     description='Enables embedding of web-contents from popular sites in MarkDown(*.md) on Lektor CMS',
@@ -14,5 +27,19 @@ setup(
         'lektor.plugins': [
             'embed-x = lektor_embed_x:EmbedXPlugin',
         ]
-    }
+    },
+    keywords=['embed', 'html', 'javascript', 'embeddable', 'code generation', 'from url'],
+    platforms='any',
+    long_description=long_description,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Utilities',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
 )
